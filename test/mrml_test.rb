1f4a24fb93bd84ff30ea8de81e6cd40911183fb6
jonian <jonian@hardpixel.eu>
2021-02-02 18:15:16 +0200

    add tests

--- test/mrml_test.rb
+++ test/mrml_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class MrmlTest < Minitest::Test
+  TPL_PATH = File.expand_path('templates', __dir__)
+
   def test_that_it_has_a_version_number
     refute_nil ::MRML::VERSION
+  end
+
+  def test_that_it_generates_title
+    result = ::MRML.to_title(valid_template)
+    assert_equal result, 'Newsletter Title'
+  end
+
+  def test_that_it_generates_preview
+    result = ::MRML.to_preview(valid_template)
+    assert_equal result, 'Newsletter Preview'
+  end
+
+  def test_that_it_generates_html
+    result = ::MRML.to_html(valid_template)
+
+    refute result.match?(%r{</?mj.+?>})
+    assert result.match?(/<body>/)
+    assert result.match?('Hello World')
+  end
+
+  def test_that_it_raises_an_exception
+    assert_raises ::MRML::Error do
+      ::MRML.to_html(invalid_template)
+    end
+  end
+
+  private
+
+  def valid_template
+    @valid_template ||= File.read(File.join(TPL_PATH, 'valid.mjml'))
+  end
+
+  def invalid_template
+    @invalid_template ||= File.read(File.join(TPL_PATH, 'invalid.mjml'))
   end
 end
