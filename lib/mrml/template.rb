537b199d23768b8dd7ad17e3d327885ce039a3fb
jonian <jonian@hardpixel.eu>
2022-06-16 18:08:36 +0300

    add support for json and hash load/dump

--- lib/mrml/template.rb
+++ lib/mrml/template.rb
@@ -1,4 +1,15 @@
+require 'json'
+
 module MRML
   class Template
+    class << self
+      def from_hash(hash)
+        from_json(JSON.generate(hash))
+      end
+    end
+
+    def to_hash
+      JSON.parse(to_json)
+    end
   end
 end
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use magnus::{
 };
 
 use mrml::mjml::MJML;
+use mrml::prelude::print::Print;
 use mrml::prelude::render::Options;
 
 fn mrml_error() -> ExceptionClass {
@@ -29,12 +30,30 @@ impl Template {
     }
   }
 
+  fn from_json(input: String) -> Result<Self, Error> {
+    match serde_json::from_str::<MJML>(&input) {
+      Ok(res) => Ok(Self { res }),
+      Err(ex) => Err(Error::new(mrml_error(), ex.to_string()))
+    }
+  }
+
   fn get_title(&self) -> Option<String> {
     self.res.get_title()
   }
 
   fn get_preview(&self) -> Option<String> {
     self.res.get_preview()
+  }
+
+  fn to_mjml(&self) -> String {
+    self.res.dense_print()
+  }
+
+  fn to_json(&self) -> Result<String, Error> {
+    match serde_json::to_string(&self.res) {
+      Ok(res) => Ok(res),
+      Err(ex) => Err(Error::new(mrml_error(), ex.to_string()))
+    }
   }
 
   fn to_html(&self) -> Result<String, Error> {
@@ -51,8 +70,13 @@ fn init() -> Result<(), Error> {
   let class = module.define_class("Template", Default::default())?;
 
   class.define_singleton_method("new", function!(Template::new, 1))?;
+  class.define_singleton_method("from_json", function!(Template::from_json, 1))?;
+
   class.define_method("title", method!(Template::get_title, 0))?;
   class.define_method("preview", method!(Template::get_preview, 0))?;
+
+  class.define_method("to_mjml", method!(Template::to_mjml, 0))?;
+  class.define_method("to_json", method!(Template::to_json, 0))?;
   class.define_method("to_html", method!(Template::to_html, 0))?;
 
   Ok(())
