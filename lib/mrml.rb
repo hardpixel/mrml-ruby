a7421835107a8d9739015548df90e61e200d73b6
jonian <jonian@hardpixel.eu>
2022-08-31 15:56:33 +0300

    add inline documentation

--- lib/mrml.rb
+++ lib/mrml.rb
@@ -11,16 +11,32 @@ require 'mrml/error'
 require 'mrml/template'
 require 'mrml/version'
 
+# Module that renders MJML templates into HTML/JSON using MRML,
+# a reimplementation of the MJML markup language in Rust.
 module MRML
   class << self
+    # Render template as HTML.
+    # @param template [String]
+    # @return (see Template#to_html)
+    # @raise (see Template#initialize)
 
     def to_html(template)
       call(:to_html, template)
     end
 
+    # Render template as JSON.
+    # @param template [String]
+    # @return (see Template#to_json)
+    # @raise (see Template#initialize)
+
     def to_json(template)
       call(:to_json, template)
     end
+
+    # Render template as Hash.
+    # @param template [String]
+    # @return (see Template#to_hash)
+    # @raise (see Template#initialize)
 
     def to_hash(template)
       call(:to_hash, template)
--- lib/mrml/template.rb
+++ lib/mrml/template.rb
@@ -3,12 +3,54 @@
 require 'json'
 
 module MRML
+  # Template object implemented in Rust that can parse MJML templates.
   class Template
     class << self
+      # @!method from_json(json)
+      # A new instance of Template from JSON.
+      # @!scope class
+      # @param json [String]
+      # @return [Template]
+      # @raise [Error] if json has invalid format
+
+      # A new instance of Template from Hash.
+      # @param hash [Hash]
+      # @return [Template]
+      # @raise [Error] if hash has invalid format
+
       def from_hash(hash)
         from_json(JSON.generate(hash))
       end
     end
+
+    # @!method initialize(mjml)
+    # A new instance of Template.
+    # @param mjml [String]
+    # @return [Template]
+    # @raise [Error] if mjml is not a valid template
+
+    # @!attribute [r] title
+    # Gets mj-title tag value.
+    # @return [String]
+
+    # @!attribute [r] preview
+    # Gets mj-preview tag value.
+    # @return [String]
+
+    # @!method to_mjml
+    # MJML representation of the template.
+    # @return [String]
+
+    # @!method to_json
+    # JSON representation of the template.
+    # @return [String]
+
+    # @!method to_html
+    # HTML representation of the template.
+    # @return [String]
+
+    # Hash representation of the template.
+    # @return [Hash]
 
     def to_hash
       JSON.parse(to_json)
